aaa437fddfb0cc86cc2e70f2e1ffe17efe122309
Francisco <franciscogalarzaj@gmail.com>
2018-11-03 01:19:12 -0300

    Add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -69,20 +69,41 @@ UserSchema.statics.findByToken = function (token) {
     })
 };
 
+UserSchema.statics.findByCredentials = function (email, password) {
+  var User = this;
+
+  return User.findOne({email}).then((user) => {
+    if (!user) {
+      return Promise.reject();
+    }
+
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          resolve(user);
+        } else {
+          reject();
+        }
+      });
+    });
+  });
+};
+
+
 UserSchema.pre('save', function (next) {
     var user = this;
-    
-    if (user.isModified()) {
-        bcrypt.genSalt(10, (err, salt) => {
-            bcrypt.hash(user.password, salt, (err, hash) => {
-                user.password = hash;
-                next();
-            });
+  
+    if (user.isModified('password')) {
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(user.password, salt, (err, hash) => {
+          user.password = hash;
+          next();
         });
+      });
     } else {
-        next();
+      next();
     }
-});
+  });
 
 var User = mongoose.model('User', UserSchema);
 
